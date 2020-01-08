5146e5d0b92c25dc5927481ee3bbcec5c34a6961
a-anatolech <andrey.anatolech@yandex.ru>
2020-01-08 16:45:33 +0300

    fixit

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,10 +19,10 @@ const Home = (props) => {
       </Helmet>
       <div className="intro pb-4">
         <div className="container">
-          <h1>Comon - Gatsby Small Business Theme.</h1>
+          <h1>SpiritAmalt$&^% - The Best Of The Best!</h1>
           <p>
-            Multiple content types using Markdown and JSON sources. Responsive design and SCSS. This
-            is a beautiful and artfully designed starting theme.
+            Multiple content types using Mk II and JSON sources. Responsive design and SCSS. This
+            is a beautiful and artfully designed starting theme. Yep!
           </p>
         </div>
       </div>
